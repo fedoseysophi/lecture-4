2d35607a4f9512286ac833ea985222d186f33d33
fedoseysophi <90565869+fedoseysophi@users.noreply.github.com>
2021-10-16 10:29:28 +0600

    Add files via upload

---
+++ задание2.py
@@ -0,0 +1,18 @@
+def bubble_sort(alist):
+    for i in range(len(alist) - 1, 0, -1):
+        no_swap = True
+        for j in range(0, i):
+            if alist[j + 1] < alist[j]:
+                alist[j], alist[j + 1] = alist[j + 1], alist[j]
+                no_swap = False
+        if no_swap:
+            return
+        
+alist = input('Введите ряд чисел:').split()
+alist = [int(x) for x in alist]
+bubble_sort(alist)
+print('Отсортированный ряд чисел: ',end='')
+print(alist)
+ 
+ 
+
